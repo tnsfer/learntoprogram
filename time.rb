39b43a0cbf3cb1a2366e224cf916bd408f381e1b
tnsfer <tnsfer@yahoo.com>
2013-08-09 00:57:23 -0700

    ch2

---
+++ time.rb
@@ -0,0 +1,16 @@
+puts 24 * 365  #Hours in a year.
+puts 24 * 366	#If it is a leap year.
+
+puts
+
+#Minutes in a decade.
+puts 60 * 24 * (365 * 10 + 2)	#If there are two leap years in the decade.
+puts 60 * 24 * (365 * 10 + 3)	#If there are three.
+
+puts
+
+puts 60 * 60 * 24 * (365 * 22 + 210)	#My age in seconds.
+
+puts
+
+puts 1111000000 / 60 / 60 / 24/ 365.0	#Chris Pine's approximate age.
