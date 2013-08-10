5ce77ba7f1d8ea1d04801277dec6cb65da564bac
tnsfer <tnsfer@yahoo.com>
2013-08-10 00:18:09 -0700

    Delete biggerbetter.rb

--- biggerbetter.rb
+++
@@ -1,5 +0,0 @@
-puts 'what\'s your favorite number?'
-fav_num = gets.chomp.to_i
-better_num = fav_num + 1
-puts 'that\'s good, but ' + better_num.to_s +
-' is just bigger than ' + fav_num.to_s + '!'
