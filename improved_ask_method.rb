a1e591f739f22673ecb95becf6e5ae2a07519911
tnsfer <tnsfer@yahoo.com>
2013-08-10 00:16:55 -0700

    ch9

---
+++ improved_ask_method.rb
@@ -0,0 +1,16 @@
+def ask question
+  while true
+    puts question
+    reply = gets.chomp.downcase
+
+    if reply == 'yes'
+      return true
+    end
+    if reply == 'no'
+      return false
+    end
+    puts 'Please answer "yes" or "no".'
+  end
+end
+
+puts ask ('Have you enjoyed learning programming?')
