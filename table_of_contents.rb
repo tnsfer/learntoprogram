b7f9f1f79452eb17f008c3b11788e8204e495d43
tnsfer <tnsfer@yahoo.com>
2013-08-10 00:12:28 -0700

    ch6

---
+++ table_of_contents.rb
@@ -0,0 +1,9 @@
+title = "Table of Contents".center(50)
+ch_1 = "Chapter 1:  Getting Started".ljust(30) + "page  1".rjust(20)
+ch_2 = "Chapter 2:  Numbers".ljust(30) + "page  9".rjust(20)
+ch_3 = "Chapter 3:  Letters".ljust(30) + "page 13".rjust(20)
+
+puts title
+puts ch_1
+puts ch_2
+puts ch_3
